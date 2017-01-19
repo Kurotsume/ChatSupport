bdd13eec97ce3b9c15428563059f75f9eda7af4f
Kurotsume <boweezy777@gmail.com>
2017-01-19 17:21:51 -0500

    deleted index.html replaced with index.php for footer reasons

---
+++ index.php
@@ -0,0 +1,66 @@
+<html>
+    <head>
+    <!--Have browsers resposize design active -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Loading jQuery from remote -->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <!--<script src="css/style.css" type="text/css"></script>-->
+    
+    <style type="text/css">
+    
+        #header{
+            background-color:#333;
+            width : 100%;
+            height:60px;
+        
+        }
+        
+        .wrapper {
+            min-height: 100%;
+            height: auto !important;
+            height: 100%;
+            margin: 0 auto -4em;
+        }
+        
+        body{
+            margin:0;
+        
+        }
+        
+        footer{
+            background-color: #333;
+            width:100%;
+            height:20px;
+            position:absoulute;
+            
+        
+        }
+        
+    </style>   
+        
+    </head>
+
+    
+    <body>
+    
+        <div class="wrapper">
+        
+            <div id="header">
+            
+            </div>
+            
+            <div id="container">
+            
+            
+            
+            </div> 
+            
+        </div>  
+        
+    </body>
+    
+    <footer id="footer">
+        <?php echo "Copyright" . date('Y') . ", Boris Thertus"; ?>
+    </footer>
+    
+</html>
